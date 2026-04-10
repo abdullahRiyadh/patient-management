9299a46c918a6401dd46527b723384f75e1fa8d3
abdullahRiyadh <abdullahriyadhcse@gmail.com>
2026-04-10 22:46:57 +0600

    fix: appointmentTime Schema type fixed and  appointmentDate validation constraint update

--- src/main/java/com/abdullah/patient_management/dto/request/AppointmentRequest.java
+++ src/main/java/com/abdullah/patient_management/dto/request/AppointmentRequest.java
@@ -1,7 +1,9 @@
 package com.abdullah.patient_management.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -21,10 +23,11 @@ public class AppointmentRequest {
     private Long doctorId;
 
     @NotNull(message = "Appointment date is required")
-    @Future(message = "Appointment date must be in the future")
+    @FutureOrPresent(message = "Appointment date must be today or in the future")
     private LocalDate appointmentDate;
 
     @NotNull(message = "Appointment time is required")
+    @Schema(type = "string", example = "10:30")
     @JsonFormat(pattern = "HH:mm")
     private LocalTime appointmentTime;
 
